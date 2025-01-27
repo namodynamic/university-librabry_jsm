773351ab49bdee118ec49fa202ab2cd34cc9d0dc
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-01-27 20:34:09 +0100

    - Create sign-in and sign-up pages with validation schemas.
    - Set up serverless Postgres database connection with Neon
    
    - Implemented layout and home page components for book overview and listing.

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import AuthForm from "@/components/AuthForm";
+import { signInSchema } from "@/lib/validation";
+
+const Page = () => (
+  <AuthForm
+    type="SIGN_IN"
+    schema={signInSchema}
+    defaultValues={{ email: "", password: "" }}
+    onSubmit={() => {}}
+  />
+);
+export default Page;
